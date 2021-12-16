456079921961743b960f9cf6b4359af5684a10a7
qiuzhanghua <qiuzhanghua@icloud.com>
2021-12-16 11:06:07 +0800

    0.2.0

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("Hello, World!")
+process = require('process')
+const os = require("os")
+const path = require('path')
+
+let TDP_HOME = process.env.TDP_HOME || os.homedir() + path.sep + 'tdp' + path.sep + 'yarn_repository'
+
+console.log(TDP_HOME)
