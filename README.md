17cb1962c95bb367a909b73cec3456c02a004c99
qiuzhanghua <qiuzhanghua@icloud.com>
2021-12-16 19:42:20 +0800

    complete

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # local-yarn
 process yarn.lock
+
+
+## Zip
+on your unix machine, under nodejs home, run
+```bash
+./gen-yarn-zip <name>
+```
+
+## Setup
+```bash
+yarn global add https://github.com/qiuzhanghua/local-yarn.git#0.5.0
+```
+
+## Install Zip
+```bash
+local-yarn install <name>.yarn.lock.zip
+```
+
+## update local yarn.lock
+```bash
+local-yarn get meta
+```
--- index.js
+++ index.js
@@ -1,15 +1,21 @@
-const VERSION = '0.4.0'
+'use strict'
+
+const VERSION = '0.5.0'
 
 const arg = require('arg')
-const process = require('process')
-const os = require("os")
+const os = require('os')
 const path = require('path')
+const adm_zip = require('adm-zip')
+const fs = require('fs')
+const lockfile = require('@yarnpkg/lockfile')
+const mkdirp = require('mkdirp')
 
-let TDP_HOME = process.env.TDP_HOME || os.homedir() + path.sep + 'tdp' + path.sep + 'yarn_repository'
+let local_yarn_repository = (process.env.TDP_HOME || os.homedir() + path.sep + 'tdp') + path.sep + 'yarn_repository'
 
-console.log(TDP_HOME)
+if (!fs.existsSync(local_yarn_repository)) {
+    mkdirp.sync(local_yarn_repository)
+}
 
-// TODO add functions
 const args = arg({
     '--help': Boolean,
     '--version': Boolean,
@@ -21,13 +27,45 @@ if (args['--help']) {
     console.log('local yarn repository tool, v' + VERSION)
 } else if (args['--version']) {
     console.log('version ' + VERSION)
-} else if (args._[0] == 'get' && args._[1] == 'meta') {
-    console.log('TODO Merge meta file to current dir"s yarn.lock')
-} else if (args._[0] == 'install') {
-    // console.log('TODO install Zip of tag to repository, and merge yarn.lock to repository')
+} else if (args._[0] === 'get' && args._[1] === 'meta') {
+    merge('yarn.lock', local_yarn_repository + path.sep + 'all.lock')
+} else if (args._[0] === 'install') {
     let filename = args._[1]
-    if (filename) {
-        // TODO unzip files ends with .tgz to TDP_HOME/yarn_repository
-        console.log("merge yarn.lock @ TDP_HOME/yarn_repository")
+    if (filename.endsWith('.yarn.lock.zip')) {
+        let unzip = new adm_zip(filename, {})
+        unzip.extractAllTo(local_yarn_repository, true, true, "")
+        merge(local_yarn_repository + path.sep + 'all.lock', local_yarn_repository + path.sep + 'yarn.lock')
+        fs.rmSync(local_yarn_repository + path.sep + 'yarn.lock')
     }
+}
+
+function merge(f1, f2) {
+    let obj = {}
+    if (fs.existsSync(f1)) {
+        let file = fs.readFileSync(f1, 'utf8')
+        let json = lockfile.parse(file)
+        if (json.type !== 'success') {
+             return false
+        }
+        obj = json.object
+    }
+
+    let obj2 = {}
+    if (fs.existsSync(f2)) {
+        let file = fs.readFileSync(f2, 'utf8')
+        let json = lockfile.parse(file)
+        if (json.type !== 'success') {
+            return false
+        }
+        obj2 = json.object
+    }
+
+    Object.assign(obj, obj2)
+
+    const fd = fs.openSync(f1, "w+")
+    let text = lockfile.stringify(obj)
+    const position = 0
+    const numberOfBytesWritten =
+        fs.writeSync(fd, text, position, 'utf8')
+    return numberOfBytesWritten > 0
 }
