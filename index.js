3077e85631d3580aea1c7bf570f1558879d5afb4
qiuzhanghua <qiuzhanghua@icloud.com>
2021-12-16 12:36:48 +0800

    0.4.0, add args parser

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-process = require('process')
+const VERSION = '0.4.0'
+
+const arg = require('arg')
+const process = require('process')
 const os = require("os")
 const path = require('path')
 
@@ -7,3 +10,24 @@ let TDP_HOME = process.env.TDP_HOME || os.homedir() + path.sep + 'tdp' + path.se
 console.log(TDP_HOME)
 
 // TODO add functions
+const args = arg({
+    '--help': Boolean,
+    '--version': Boolean,
+    '-v': '--version',
+    '-h': '--help'
+});
+
+if (args['--help']) {
+    console.log('local yarn repository tool, v' + VERSION)
+} else if (args['--version']) {
+    console.log('version ' + VERSION)
+} else if (args._[0] == 'get' && args._[1] == 'meta') {
+    console.log('TODO Merge meta file to current dir"s yarn.lock')
+} else if (args._[0] == 'install') {
+    // console.log('TODO install Zip of tag to repository, and merge yarn.lock to repository')
+    let filename = args._[1]
+    if (filename) {
+        // TODO unzip files ends with .tgz to TDP_HOME/yarn_repository
+        console.log("merge yarn.lock @ TDP_HOME/yarn_repository")
+    }
+}
